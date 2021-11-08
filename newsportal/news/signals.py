bae02579383b6cdb658d140923de879d63f9e22e
Serge <sergey@batalov.email>
2021-11-08 11:15:00 +0000

    Пользователя под редактированием и созданием научились выдирать Список подписчиков тоже выдираем Поправил отправку емылов. дело было не в бобине. Никак не прикрепить html

--- newsportal/news/signals.py
+++ newsportal/news/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
 from .models import Post, Subscriber
 from .secda import admail
 
@@ -14,13 +16,18 @@ def notify_subscribers_publication(sender, instance, created, **kwargs):
 
     list_of_dictcs= list(Subscriber.objects.filter(category=1).values('user__email'))
     list_of_subscribers = [d['user__email'] for d in list_of_dictcs if 'user__email' in d]
-    print(list_of_subscribers)
+    print(instance.id)
 
+    pub_updates = render_to_string('email/pub_updates.html', {'context': 'values'})
     # отправляем письмо
     msg = EmailMultiAlternatives(
         subject=subject,
-        body=f'Уважаемый подписчик, в интересующих Вас категориях произошли изменения',  # сообщение с кратким описанием
+        # body=f'Уважаемый подписчик, в интересующих Вас категориях произошли изменения. Можете перейти по ссылке http://127.0.0.1:8000/news/{instance.id}',  # сообщение с кратким описанием
         from_email=admail,  # здесь указываете почту, с которой будете отправлять
         to=list_of_subscribers, # здесь список получателей. Например, секретарь, сам врач и т. д.
     )
+    msg.attach_alternative(pub_updates, "text/html")
+    msg.content_subtype = "html"
+    print("BODY: ", msg.body)
+    print("MESSAGE : ", msg.message())
     msg.send()
